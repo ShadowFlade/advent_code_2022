8e9898ff7cc346bc41202f52b4abed8669f3ccaf
Vladimir Drugov <wolffdrugov@yandex.ru>
2023-11-13 21:10:50 +0300

    day1:closer to solution

--- day_1/solution.js
+++ day_1/solution.js
@@ -1,15 +1,18 @@
+const { log } = require('console');
 const fs = require('fs');
 let data = fs.readFileSync('./data.txt',{encoding: 'utf8',flag:'r'});
 let maxSum = 0;
-data = data.split(' ');
-console.log(Array.isArray(data),' is');
+data = data.split(/\r*\n/);
+console.log(data.length,' is');
 let indexOfElf;
 data.forEach((elf,index) => {
     let caloriesSum = 0;
 
     elf.split('\n').forEach((calories) => {
+        log(calories,' calories');
         caloriesSum+=+calories
     }); 
+    console.log(caloriesSum,' maxSum');
     if(caloriesSum > maxSum){
         maxSum = caloriesSum
         indexOfElf = index;
