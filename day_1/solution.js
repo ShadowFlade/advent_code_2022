ae17b072302a7fdc7e648dd53fed55a14fc73e3a
Vladimir Drugov <wolffdrugov@yandex.ru>
2023-11-15 23:51:25 +0300

    update

--- day_1/solution.js
+++ day_1/solution.js
@@ -1,23 +1,19 @@
-const { log } = require('console');
-const fs = require('fs');
-let data = fs.readFileSync('./data.txt',{encoding: 'utf8',flag:'r'});
+const { log } = require("console");
+const fs = require("fs");
+let data = fs.readFileSync("./data.txt", { encoding: "utf8", flag: "r" });
 let maxSum = 0;
-data = data.split(/\r*\n/);
-console.log(data.length,' is');
+data = data.split(/\r/);
+console.log(data.length, " is");
 let indexOfElf;
-data.forEach((elf,index) => {
-    let caloriesSum = 0;
+data.forEach((elf, index) => {
+	let caloriesSum = 0;
+        log(caloriesSum,' calories sum');
+	if (caloriesSum > maxSum) {
+		maxSum = caloriesSum;
+		indexOfElf = index;
+	}
+});
+console.log(data,' data');
 
-    elf.split('\n').forEach((calories) => {
-        log(calories,' calories');
-        caloriesSum+=+calories
-    }); 
-    console.log(caloriesSum,' maxSum');
-    if(caloriesSum > maxSum){
-        maxSum = caloriesSum
-        indexOfElf = index;
-    }
-})
-console.log(indexOfElf,' last index of elf');
-return indexOfElf;
-
+console.log(maxSum, " last index of elf");
+return maxSum;
