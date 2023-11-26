318317714be9f9174262a8111750534ab1552fc1
Vladimir Drugov <wolffdrugov@yandex.ru>
2023-11-26 23:02:53 +0300

    master:day3 started

--- day_1/day3/part1/solution.ts
+++ day_1/day3/part1/solution.ts
@@ -1,2 +1,5 @@
 import * as fs from 'fs';
 import * as path from 'path';
+const filePath = path.resolve(__dirname,'../../data.txt');
+let data = fs.readFileSync(filePath,{encoding:'utf8'});
+const someData = data.split('\n');
