d2f001c64a53b9f69be5bda12e0e1244ae5af27e
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-01-10 21:45:06 +0300

    day3part2: solved with 1st try wow

--- day3/part2/solution.ts
+++ day3/part2/solution.ts
@@ -31,33 +31,48 @@ function solution32() {
 		"y",
 		"z",
 	];
-    const elfGroups = [];
-    let elfTempGroup = [];
-    for(let i = 0; i < data.length; i ++){
-       elfTempGroup.push(data[i]);
-        if(elfTempGroup.length == 3){
-            elfGroups.push(elfTempGroup)
-        }
-    }
+	const elfGroups = [];
+	let elfTempGroup = [];
+	for (let i = 0; i < data.length; i++) {
+		elfTempGroup.push(data[i]);
+		if (elfTempGroup.length == 3) {
+			elfGroups.push(elfTempGroup);
+            elfTempGroup = [];
+		}
+	}
+    console.log(elfGroups,' groups')
 	function calcPriority(letter: string): number {
 		const isUpperCase =
 			alphabet.includes(letter.toLowerCase()) &&
 			!alphabet.includes(letter);
 		const index =
 			alphabet.indexOf(isUpperCase ? letter.toLowerCase() : letter) + 1;
+        console.log(index,' index');
 		const upperCaseMult = isUpperCase ? 26 : 0;
 		return index + upperCaseMult;
 	}
 	let sum = 0;
-    let groupCounter = 1;
+    let counter = 0;
+    console.log(elfGroups.length,' length');
 	for (const group of elfGroups) {
 		let repeatingLetter = "";
-        group[0].forEach(letter => {
-        if(group[1].includes(letter) && group[2].includes(letter){
-            repeatingLetter = letter;
-        })}
-
-        )}
+		group[0].split("").forEach((letter: string) => {
+			if (
+				group[1].includes(letter) &&
+				group[2].includes(letter) &&
+				repeatingLetter == ""
+			) {
+				repeatingLetter = letter;
+			}
+		});
+		const points = calcPriority(repeatingLetter);
+        console.log(points,' points')
+		sum += points;
+		repeatingLetter = "";
+        counter += 1;
 	}
+	console.log(sum, " smth");
+    console.log(counter,' counter');
 }
+solution32();
 export default solution32;
