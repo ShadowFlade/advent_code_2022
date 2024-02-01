d016db1380f7436489d7e94dab9511019f609c15
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-02-01 21:05:07 +0300

    fixed fucking bug with the wrong node traversing, now need to calc not like and idiot

--- day7/part1/solution.ts
+++ day7/part1/solution.ts
@@ -6,48 +6,58 @@ export default function main() {
 	const data = fs.readFileSync(filepath, { encoding: "utf8" }).split("\n");
 	let currentDirectory = "";
 	const directories = {};
-	for (let i = 0; i < data.length - 1; i++) {
+    let nextCommandIndex = 0;
+	for (let i = 0; i < data.length - 1 && nextCommandIndex >= 0; i++) {
 		if (data[i][0] == "$") {
 			const parsed = data[i].match(/\$ (\w+)(?:$| (.+))/);
 			const command = parsed[1];
+
+			nextCommandIndex =
+				data.findIndex((item, index) => index > i && item[0] == "$") -
+				1 - +i;
+
 			switch (command) {
 				case "cd":
 					if (parsed[2] == "..") {
-						currentDirectory = directories[parsed[2]].prev;
+						currentDirectory =
+							directories[currentDirectory].prev.name;
 					} else if (parsed[2] == "/") {
-						directories["/"] = {weight: 0};
+						directories["/"] = { weight: 0, name: "/" };
 						currentDirectory = "/";
 					} else {
 						const newDirectory = {
 							prev: directories[currentDirectory],
-                            weight: 0
+							weight: 0,
+							name: parsed[2],
 						};
 						directories[currentDirectory][parsed[2]] = newDirectory;
 						directories[parsed[2]] = newDirectory;
+						currentDirectory = parsed[2];
 					}
-					currentDirectory = parsed[2];
 					break;
 				case "ls":
-					const nextCommandIndex = data.findIndex(
-						(item, index) => index > i && item[0] == "$"
-					) - 1;
-					const filesAndDirectories = data.slice(i + 1, nextCommandIndex);
+					const filesAndDirectories = data.slice(
+						i + 1,
+						nextCommandIndex
+					);
 					//redundant copying, we can just filter out indexes and give calc function links to objects but whatever im lazy
 					const files = [];
 					filesAndDirectories.forEach((item) => {
 						if (item.slice(0, 3) != "dir") {
 							files.push(item);
-						} else if (item.slice(0,3) == 'dir'){
-                            const newDir = {};
-                            directories[item.slice(5)] = newDir;
-                            directories[currentDirectory][item.slice(5)] = newDir;
-                        }
+						} else if (item.slice(0, 3) == "dir") {
+							const newDir = {};
+							directories[item.slice(5)] = newDir;
+							directories[currentDirectory][item.slice(5)] =
+								newDir;
+						}
 					});
-					i += nextCommandIndex;
 					const dirWeight = calcDirectoryWeight(files);
 					directories[currentDirectory].weight += +dirWeight;
 					break;
 			}
+
+			i += nextCommandIndex;
 		}
 	}
 	let dirSum = 0;
@@ -55,13 +65,16 @@ export default function main() {
 		if (item == "/") {
 			return;
 		} else {
-            const dirWeight = +directories[item].weight;
-            if(typeof dirWeight == 'number' && !isNaN(dirWeight)){
-                dirSum += dirWeight
-            }
+            console.log(directories[item], item);
+			const dirWeight = +directories[item].weight;
+            console.log(dirWeight,' dirweight');
+			if (typeof dirWeight == "number" && !isNaN(dirWeight)) {
+				dirSum += dirWeight;
+                console.log(dirSum,' oh yes');
+			}
 		}
 	});
-    console.log(dirSum,' sum')
+	console.info(dirSum, " sum");
 }
 
 function calcDirectoryWeight(files) {
