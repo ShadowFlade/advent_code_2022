14b432131a175756f0104f7ef0e534134a64a09c
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-02-02 20:40:02 +0300

    update

--- day7/part1/solution.ts
+++ day7/part1/solution.ts
@@ -1,21 +1,21 @@
 import * as fs from "fs";
 import * as path from "path";
-
+//we need to recursively update the weight of prev directories (parent directories)
 export default function main() {
 	const filepath = path.resolve(__dirname, "./data.txt");
 	const data = fs.readFileSync(filepath, { encoding: "utf8" }).split("\n");
 	let currentDirectory = "";
 	const directories = {};
-    let nextCommandIndex = 0;
+	let nextCommandIndex = 0;
 	for (let i = 0; i < data.length - 1 && nextCommandIndex >= 0; i++) {
 		if (data[i][0] == "$") {
 			const parsed = data[i].match(/\$ (\w+)(?:$| (.+))/);
 			const command = parsed[1];
-
 			nextCommandIndex =
 				data.findIndex((item, index) => index > i && item[0] == "$") -
-				1 - +i;
-
+				1 -
+				+i;
+			console.log(nextCommandIndex, " next command index");
 			switch (command) {
 				case "cd":
 					if (parsed[2] == "..") {
@@ -38,12 +38,14 @@ export default function main() {
 				case "ls":
 					const filesAndDirectories = data.slice(
 						i + 1,
-						nextCommandIndex
+						i + nextCommandIndex
 					);
 					//redundant copying, we can just filter out indexes and give calc function links to objects but whatever im lazy
 					const files = [];
+					console.log(filesAndDirectories, " files and directories");
 					filesAndDirectories.forEach((item) => {
 						if (item.slice(0, 3) != "dir") {
+							console.log(item.slice(0, 3), " not dir?");
 							files.push(item);
 						} else if (item.slice(0, 3) == "dir") {
 							const newDir = {};
@@ -52,6 +54,7 @@ export default function main() {
 								newDir;
 						}
 					});
+					console.log(files, " calcing files");
 					const dirWeight = calcDirectoryWeight(files);
 					directories[currentDirectory].weight += +dirWeight;
 					break;
@@ -65,12 +68,14 @@ export default function main() {
 		if (item == "/") {
 			return;
 		} else {
-            console.log(directories[item], item);
-			const dirWeight = +directories[item].weight;
-            console.log(dirWeight,' dirweight');
-			if (typeof dirWeight == "number" && !isNaN(dirWeight)) {
-				dirSum += dirWeight;
-                console.log(dirSum,' oh yes');
+			const newWeight: number = +directories[item].weight;
+
+			if (
+				typeof newWeight == "number" &&
+				!isNaN(newWeight) &&
+				newWeight <= 100_000
+			) {
+				dirSum += newWeight;
 			}
 		}
 	});
@@ -80,6 +85,10 @@ export default function main() {
 function calcDirectoryWeight(files) {
 	let sum = 0;
 	for (let i = 0; i < files.length; i++) {
+        console.log('123123',files[i]);
+		if (files[i].includes("vcq.flg")) {
+			console.log(files[i], " file");
+		}
 		const fileSize = files[i].match(/\d+/)[0];
 		sum += +fileSize;
 	}
